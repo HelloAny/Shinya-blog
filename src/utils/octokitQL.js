ea2374d477a5974954ddd7340b23e56a23ac5879
AiYiNing <1914420862@qq.com>
2020-09-03 23:18:15 +0800

    init

--- src/utils/octokitQL.js
+++ src/utils/octokitQL.js
@@ -1,35 +1,33 @@
 import { graphql } from "@octokit/graphql"
 import { REPO_AUTHOR, OWNER, REPOS } from "../constants/auth"
 
-// const fetchQL = () => {
-
-onmessage = async (e) => {
-  const { payload, schema } = e.data
-  console.log(this.REPO_AUTHOR)
-  try {
-    const { repository } = graphql(schema, {
-      owner: OWNER,
-      repo: REPOS,
-      ...payload
-    },
-      {
-        headers: {
-          authorization: `token ${REPO_AUTHOR}`
-        }
-      });
-    console.log("repository", repository)
-    postMessage(repository)
-  } catch (error) {
-    console.log("error", error)
-    postMessage(error)
+const fetchQL = () => {
+  onmessage = async (e) => {
+    const { payload, schema } = e.data
+    try {
+      const { repository } = graphql(schema, {
+        owner: OWNER,
+        repo: REPOS,
+        ...payload
+      },
+        {
+          headers: {
+            authorization: `token ${REPO_AUTHOR}`
+          }
+        });
+      console.log(repository)
+      postMessage(repository)
+    } catch (error) {
+      console.log("error", error)
+      postMessage(error)
+    }
   }
 }
-// }
 
-// let code = fetchQL.toString();
-// code = code.substring(code.indexOf("{") + 1, code.lastIndexOf("}"));
+let code = fetchQL.toString();
+code = code.substring(code.indexOf("{") + 1, code.lastIndexOf("}"));
 
-// const blob = new Blob([code], { type: "application/javascript" });
-// const worker_script = URL.createObjectURL(blob);
+const blob = new Blob([code], { type: "application/javascript" });
+const worker_script = URL.createObjectURL(blob);
 
-// export default worker_script
+export default worker_script
