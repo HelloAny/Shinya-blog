58ec088f1024274208da94658c133ee9f9ebbc34
AiYiNing <1914420862@qq.com>
2020-10-26 22:26:43 +0800

    Update auth.js

--- src/constants/auth.js
+++ src/constants/auth.js
@@ -1,4 +1,4 @@
-export const REPO_AUTHOR = "25b8de6496b34766fc90438c3a1a2efbd713f0d2"
+export const REPO_AUTHOR = "792a71d6d3b927d50881833a27ed3e82ccbef7ca"
 
 export const OWNER = "HelloAny"
 export const REPOS = "article"
