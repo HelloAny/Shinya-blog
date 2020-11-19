6c3a8581f0861acab41a22483b64161763a3c837
AiYiNing <1914420862@qq.com>
2020-11-19 23:09:07 +0800

    Revert "update"
    This reverts commit a71b40671174d82aec038b216e19b3c9870dcfea.

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,3 @@
 const { override, addBabelPlugin } = require('customize-cra')
 
-module.exports = override(addBabelPlugin(['react-activation/babel', "@babel/plugin-proposal-decorators", { "legacy": true }]))
+module.exports = override(addBabelPlugin('react-activation/babel'))
