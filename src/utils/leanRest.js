d4ad1b310060e9b2e4636101d355af431e31331e
AiYiNing <1914420862@qq.com>
2021-02-06 12:57:56 +0800

    style: leanRest

--- src/utils/leanRest.js
+++ src/utils/leanRest.js
@@ -1,45 +1,59 @@
-import AV from "leancloud-storage"
-import { appId, appKey, serverURL } from '../constants/auth'
+import AV from "leancloud-storage";
+import { appId, appKey, serverURL } from "../constants/auth";
 
 AV.init({
   appId,
   appKey,
-  serverURL
+  serverURL,
 });
 
 async function fetch(url, payload, method) {
   const query = new AV.Query(url);
   if (!payload) {
-    return method ? query[method]() : query.find()
+    return method ? query[method]() : query.find();
   }
-  const { limit, skip, labels, labelsBy, pin = true, sort, sortBy, select, equal, equalVal } = payload
-  const arrayString = val => {
-    if (val && typeof val === 'string') return val
-    return val.shift()
-  }
+  const {
+    limit,
+    skip,
+    labels,
+    labelsBy,
+    pin = true,
+    sort,
+    sortBy,
+    select,
+    equal,
+    equalVal,
+  } = payload;
+  const arrayString = (val) => {
+    if (val && typeof val === "string") return val;
+    return val.shift();
+  };
 
-  limit && query.limit(limit)
-  skip && query.skip(skip)
-  select && query.select(select)
-  equal && equalVal && query.equalTo(equal, equalVal)
-  !!pin && query.addDescending('pin')
+  limit && query.limit(limit);
+  skip && query.skip(skip);
+  select && query.select(select);
+  equal && equalVal && query.equalTo(equal, equalVal);
+  !!pin && query.addDescending("pin");
   if (sort && sort === true && sortBy) {
-    query.addAscending(sortBy)
+    query.addAscending(sortBy);
   } else if (sort && sort === true) {
-    query.addDascending('ct')
+    query.addDascending("ct");
   } else {
-    query.addDescending('ct');
+    query.addDescending("ct");
   }
 
   if (labels && labelsBy) {
-    if (typeof labelsBy === 'string' || (Array.isArray(labelsBy) && labelsBy.length === 1)) {
-      query.equalTo(labels, arrayString(labelsBy))
+    if (
+      typeof labelsBy === "string" ||
+      (Array.isArray(labelsBy) && labelsBy.length === 1)
+    ) {
+      query.equalTo(labels, arrayString(labelsBy));
     } else if (Array.isArray(labelsBy) && labelsBy.length > 1) {
-      query.equalTo(labels, labelsBy)
+      query.equalTo(labels, labelsBy);
     }
   }
 
-  return method ? query[method]() : query.find()
+  return method ? query[method]() : query.find();
 }
 
-export default fetch
+export default fetch;
